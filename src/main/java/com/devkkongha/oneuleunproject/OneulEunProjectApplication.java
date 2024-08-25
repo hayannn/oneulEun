b625b3609655da5ac29ff442c280a6776252c025
hayannn <dlgkdis801@naver.com>
2024-08-25 23:36:12 +0900

    feat: oneulEun 프로젝트 생성 및 연동 완료

---
+++ src/main/java/com/devkkongha/oneuleunproject/OneulEunProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devkkongha.oneuleunproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulEunProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneulEunProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devkkongha/oneuleunproject/OneulEunProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkkongha.oneuleunproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneulEunProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
